2067452df594c000ba925f752c3ef1d48db441e3
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-03-07 14:04:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,31 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "East US"
+# Configure Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.59.0"
+    } 
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = "true"
+  
+  # Connection to Azure
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
+
+variable "prefix" {
+  default = "terraform"
+}
+
+resource "azurerm_resource_group" "rg90" {
+  name     = "resource_group_90"
+  location = "Central India"
 }

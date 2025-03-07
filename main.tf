dee1ccfa4d77cfa2bcacc926008772fac847fd9c
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-03-07 13:54:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-terraform {
-  backend "remote" {
-    organization = "april-org"  # Replace with your Terraform Cloud organization name
-
-    workspaces {
-      name = "april02ci-cd"  # Replace with your workspace name
-    }
-  }
+# Configure the Azure Provider
+provider "azurerm" {
+  features {}
 }
 resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"

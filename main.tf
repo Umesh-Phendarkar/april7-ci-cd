4f1e153e51158195105860da3dd22efe439fc872
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-03-17 00:02:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Define the Azure Resource Group Updated token
+# Define the Azure Resource Group 
 resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"
   location = "East US"
